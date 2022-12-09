037c62c40010af6d1bfb4059f3419fbf4cae3306
dunnoconnor <michael.dunn.oconnor@gmail.com>
2022-12-09 17:02:23 -0500

    day 9 refacto

--- day9/rope.py
+++ day9/rope.py
@@ -8,14 +8,13 @@ class Rope:
 
     def gen_nodes(self,size) -> list[Pos]:
         nodes = []
-        for i in range(size):
+        for _ in range(size):
             nodes.append(Pos(0,0))
         return nodes
 
     def move_head(self,line:str):
-        dir = line[0]
-        val = int(line[1:])
-        for i in range(val):
+        dir, val = line.split(" ")
+        for _ in range(int(val)):
             match dir:
                 case "U":
                     self.nodes[0].y += 1
@@ -31,35 +30,34 @@ class Rope:
                     self.chase(1)
             self.visited.append(str(self.nodes[-1].x)+"*"+str(self.nodes[-1].y))
 
+    
+    def shift(self, n, d):
+        if d == "x":
+            if(self.nodes[n].x < self.nodes[n-1].x):
+                self.nodes[n].x +=1
+            elif(self.nodes[n].x > self.nodes[n-1].x):
+                    self.nodes[n].x -=1
+        elif d == "y":
+            if(self.nodes[n].y < self.nodes[n-1].y):
+                self.nodes[n].y +=1
+            elif(self.nodes[n].y > self.nodes[n-1].y):
+                self.nodes[n].y -=1
+
     def chase(self, n:int):
         if (self.nodes[n].y < self.nodes[n-1].y-1):
                 self.nodes[n].y +=1
-                if(self.nodes[n].x < self.nodes[n-1].x):
-                    self.nodes[n].x +=1
-                elif(self.nodes[n].x > self.nodes[n-1].x):
-                    self.nodes[n].x -=1
+                self.shift(n,"x")
         if (self.nodes[n].y > self.nodes[n-1].y+1):
                 self.nodes[n].y -=1
-                if(self.nodes[n].x < self.nodes[n-1].x):
-                    self.nodes[n].x +=1
-                elif(self.nodes[n].x > self.nodes[n-1].x):
-                    self.nodes[n].x -= 1
+                self.shift(n,"x")
         if self.nodes[n].x > self.nodes[n-1].x+1:
                 self.nodes[n].x -=1
-                if(self.nodes[n].y < self.nodes[n-1].y):
-                    self.nodes[n].y +=1
-                elif(self.nodes[n].y > self.nodes[n-1].y):
-                    self.nodes[n].y -=1
+                self.shift(n,"y")
         if self.nodes[n].x < self.nodes[n-1].x-1:
                 self.nodes[n].x +=1
-                if(self.nodes[n].y < self.nodes[n-1].y):
-                    self.nodes[n].y +=1
-                elif(self.nodes[n].y > self.nodes[n-1].y):
-                    self.nodes[n].y -=1
+                self.shift(n,"y")
         if (n+1)<len(self.nodes):
             self.chase(n+1)
-    
+
     def get_visited(self) -> int:
-        print(self.visited)
-        print(set(self.visited))
         return len(set(self.visited))
